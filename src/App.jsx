99a5698698ad0c4ec96ec85bc8e4bfb98f670970
Jonathan Cañola Salazar <jocasa90@hotmail.com>
2025-09-03 15:29:22 -0500

    The Input.jsx component is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Title from "./components/Title/Title";
+import Input from "./components/Input/Input";
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <div className="bg-blue-400 min-h-screen text-white flex flex-col items-center justify-center px-4">
         <div className="container flex flex-col max-w-md">
           <Title title="TODO APP" />
+          <Input/>
         </div>
       </div>
     </>
---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Input = () => {
+  return (
+    <div className='mt-6 relative'>
+      <div className='absolute top-0 left-0 w-12 h-12 flex items-center justify-center pointer-events-none'>
+        <span className='border border-gray-500 focus-within:border-blue-500 w-5 h-5 rounded-full flex items-center justify-center'></span>
+      </div>
+      <input
+        type="text"
+        className='w-full pl-12 pr-4 py-3 bg-gray-700 rounded-md focus:outline-none focus:shadow-lg focus:border-blue-500 transition-all duration-300 ease-in-out placeholder:italic font-inter'
+        placeholder="Nueva tarea"
+      />
+    </div>
+  )
+}
+
+export default Input
