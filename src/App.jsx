9cf2e2632e6534e265d4ea888e3423a35735f8b6
Jonathan Cañola Salazar <jocasa90@hotmail.com>
2025-09-03 14:47:57 -0500

    The main containers of the application are created within App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ function App() {
  
 
   return (
-    <>
-      <main className="min-h-screen bg-gray-50 flex items-center justify-center p-6">
-        <h1 className="text-3xl font-bold text-blue-600">Tailwind v3 OK 🚀</h1>
-      </main>
+    < >
+      <div className="bg-blue-400 min-h-screen text-white flex flex-col items-center justify-center px-4">
+        <div className="container flex flex-col max-w-md">
+          
+        </div>
+      </div>
     </>
   );
 }
