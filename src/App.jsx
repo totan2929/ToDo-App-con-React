334b86d97f8a68ce19d7ea567e32817122f610df
Jonathan Cañola Salazar <jocasa90@hotmail.com>
2025-09-05 11:22:05 -0500

    The Todo.jsx component is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import Title from "./components/Title/Title";
 import Input from "./components/Input/Input";
 import TodoList from "./components/TodoList/TodoList";
+import Todo from "./components/Todo/Todo";
 
 function App() {
   return (
     <>
-      <div className="bg-blue-400 min-h-screen text-white flex flex-col items-center justify-center px-4">
+      <div className="bg-blue-400 min-h-screen font-inter text-white flex flex-col items-center justify-center px-4">
         <div className="container bg-blue-300 flex flex-col max-w-md">
           <Title title="TODO APP" />
           <Input/>
           <TodoList>
-            <h2>Todo list</h2>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio repudiandae magni sed corporis deleniti natus asperiores soluta iure? Odio sint soluta sunt dolore quam eum ipsam incidunt voluptas eos. Nisi?
-            Eaque aut, vitae natus optio placeat quae. Omnis harum modi optio animi dolorum similique molestias, perferendis maxime necessitatibus saepe cupiditate, accusantium blanditiis voluptatibus minima, labore sapiente reiciendis ipsam quas pariatur.</p>
+            <Todo/>
+            <Todo/>
+            <Todo/>
+            <Todo/>
           </TodoList>
         </div>
       </div>
---
+++ src/components/Todo/Todo.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Todo = ({todo}) => {
+  return (
+    <div className='flex items-center justify-between bg-gray-600 border-b border-solid border-gray-500 p-4'>
+        <div className='flex items-center'>
+            <span className='border-solid border border-gray-500 rounded-full w-5 h-5 cursor-pointer'></span>
+            <p className='pl-3'>
+              todo item
+            </p>
+            <div className='font-inter'>Title</div>
+            <img src="./delete.svg" alt="Delete" className='w-5 h-5 cursor-pointer transition-all duration-300 ease-in ' />
+        </div>
+    </div>
+  )
+}
+
+export default Todo
